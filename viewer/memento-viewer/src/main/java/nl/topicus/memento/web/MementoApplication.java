3df314a52ffcc39f6084b24a3786a8ca0048661d
Jurgen Kleverwal <jurgen.kleverwal@protopics.nl>
2014-01-31 13:36:53 +0100

    Externe storage nu oversteund

--- viewer/memento-viewer/src/main/java/nl/topicus/memento/web/MementoApplication.java
+++ viewer/memento-viewer/src/main/java/nl/topicus/memento/web/MementoApplication.java
@@ -1,11 +1,12 @@
 package nl.topicus.memento.web;
 
-import de.agilecoders.wicket.core.Bootstrap;
-import de.agilecoders.wicket.core.settings.BootstrapSettings;
-import nl.topicus.memento.web.page.HomePage;
-import nl.topicus.memento.web.page.LoginPage;
+import java.io.File;
 
 import javax.inject.Singleton;
+
+import nl.topicus.memento.videoservice.VideoService;
+import nl.topicus.memento.web.page.HomePage;
+import nl.topicus.memento.web.page.LoginPage;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
@@ -15,15 +16,27 @@ import org.apache.wicket.markup.MarkupStream;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.resolver.IComponentResolver;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.IResource;
+import org.apache.wicket.request.resource.ResourceStreamResource;
+import org.apache.wicket.request.resource.SharedResourceReference;
+import org.apache.wicket.util.resource.FileResourceStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wicketstuff.shiro.annotation.AnnotationsShiroAuthorizationStrategy;
 import org.wicketstuff.shiro.authz.ShiroUnauthorizedComponentListener;
 import org.wicketstuff.shiro.page.LogoutPage;
 
+import com.google.inject.Inject;
+
+import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.settings.BootstrapSettings;
+
 @Singleton
 public class MementoApplication extends WebApplication
 {
+	@Inject
+	private VideoService videoservice;
 
 	private static final Logger logger = LoggerFactory.getLogger(MementoApplication.class);
 
@@ -35,7 +48,10 @@ public class MementoApplication extends WebApplication
 
 		getMarkupSettings().setStripWicketTags(true);
 
-        Bootstrap.install(this, new BootstrapSettings());
+		getSharedResources().add("video", new FolderContentResource(videoservice.getStorageFolder()));
+		mountResource("video", new SharedResourceReference("video"));
+
+		Bootstrap.install(this, new BootstrapSettings());
 
 		{
 			AnnotationsShiroAuthorizationStrategy strategy = new AnnotationsShiroAuthorizationStrategy();
@@ -59,9 +75,29 @@ public class MementoApplication extends WebApplication
 			}
 		});
 
-        this.getResourceSettings().setThrowExceptionOnMissingResource(false);
+		this.getResourceSettings().setThrowExceptionOnMissingResource(false);
 
 		mountPage("/logout", LogoutPage.class);
+	}
+
+	static class FolderContentResource implements IResource
+	{
+		private final File rootFolder;
+
+		public FolderContentResource(File rootFolder)
+		{
+			this.rootFolder = rootFolder;
+		}
+
+		public void respond(Attributes attributes)
+		{
+			PageParameters parameters = attributes.getParameters();
+			String fileName = parameters.get(0).toString();
+			File file = new File(rootFolder, fileName);
+			FileResourceStream fileResourceStream = new FileResourceStream(file);
+			ResourceStreamResource resource = new ResourceStreamResource(fileResourceStream);
+			resource.respond(attributes);
+		}
 	}
 
 	@Override
