ac8a0a5378bc14bad4f3f2860f6723d8fa7be3a0
Ilsan Tijhuis <ilsan.tijhuis@protopics.nl>
2014-01-31 12:05:01 +0100

    opslaan van filmpjes mogelijk + opslaan in db

--- viewer/memento-viewer/src/main/java/nl/topicus/memento/rest/MementoResource.java
+++ viewer/memento-viewer/src/main/java/nl/topicus/memento/rest/MementoResource.java
@@ -1,6 +1,6 @@
 package nl.topicus.memento.rest;
 
-import java.io.*;
+import java.io.InputStream;
 
 import javax.inject.Singleton;
 import javax.ws.rs.Consumes;
@@ -11,12 +11,10 @@ import javax.ws.rs.core.MediaType;
 
 import nl.topicus.memento.videoservice.VideoService;
 
-import org.jooq.DSLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
-import com.google.inject.Provider;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 
@@ -30,22 +28,6 @@ public class MementoResource
 	@Inject
 	private VideoService videoService;
 
-	@Inject
-	private Provider<DSLContext> contextProvider;
-
-	// @POST
-	// @Consumes(MediaType.MULTIPART_FORM_DATA)
-	// public String onPost(@QueryParam("id") String gameId, @QueryParam("callbackPoint") String video)
-	// {
-	// File gameRoot = new File(storageFolder, gameId);
-	//
-	// DSLContext context = contextProvider.get();
-	//
-	// context.insertInto(Tables.VIDEO).set(Tables.VIDEO.ID, "...").execute();
-	//
-	// return "ok";
-	// }
-
 	@POST
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	public String onPost(@FormDataParam("file") InputStream uploadedInputStream,
@@ -53,37 +35,11 @@ public class MementoResource
 
 	{
 		String fileLocation = "Users/ilsantijhuis/workspaceDJDD/output/" + fileDetail.getFileName();
-
-		DSLContext context = contextProvider.get();
+		// videoService.saveToDatabase(uploadedInputStream, fileDetail);
+		videoService.saveToStorage(uploadedInputStream, fileDetail);
 
 		// context.insertInto(Tables.VIDEO).set(Tables.VIDEO.ID, "videovalueid").execute();
-
-		return "ok";
-	}
-
-	// save uploaded file to new location
-	private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation)
-	{
-
-		try
-		{
-			int read = 0;
-			byte[] bytes = new byte[1024];
-
-			OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
-			while ((read = uploadedInputStream.read(bytes)) != -1)
-			{
-				out.write(bytes, 0, read);
-			}
-			out.flush();
-			out.close();
-		}
-		catch (IOException e)
-		{
-
-			e.printStackTrace();
-		}
-
+		return "OK";
 	}
 
 }
--- viewer/memento-viewer/src/main/java/nl/topicus/memento/videoservice/VideoService.java
+++ viewer/memento-viewer/src/main/java/nl/topicus/memento/videoservice/VideoService.java
@@ -1,15 +1,64 @@
 package nl.topicus.memento.videoservice;
 
+import java.io.*;
+
+import nl.topicus.memento.db.Tables;
+
+import org.jooq.DSLContext;
+
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
-
-import java.io.File;
+import com.sun.jersey.core.header.FormDataContentDisposition;
 
 @Singleton
 public class VideoService
 {
 	@Inject
+	private Provider<DSLContext> contextProvider;
+
+	@Inject
 	@Named("storage.folder")
 	private File storageFolder;
+
+	public void saveToDatabase(InputStream uploadedInputStream, FormDataContentDisposition fileDetail)
+	{
+		DSLContext context = contextProvider.get();
+		context.insertInto(Tables.VIDEO).set(Tables.VIDEO.FILENAME, fileDetail.getFileName())
+			.set(Tables.VIDEO.VIDEONAME, fileDetail.getName()).set(Tables.VIDEO.MAPLOCATION, "../assets/var")
+			.set(Tables.VIDEO.LENGTH, 1000).set(Tables.VIDEO.BROWSERTYPE, "Chrome").execute();
+
+	}
+
+	public void saveToStorage(InputStream uploadedInputStream, FormDataContentDisposition fileDetail)
+	{
+		String filelocation = fileDetail.getFileName();
+		writeToFile(uploadedInputStream, filelocation);
+	}
+
+	// save uploaded file to new location
+	private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation)
+	{
+
+		try
+		{
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			OutputStream out = new FileOutputStream(new File(storageFolder.getPath() + "/" + uploadedFileLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1)
+			{
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		}
+		catch (IOException e)
+		{
+
+			e.printStackTrace();
+		}
+
+	}
 }
